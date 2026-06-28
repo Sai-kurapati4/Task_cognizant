abfdbdddc7b0e3e86bcd4b79a891b6fb0f79ae1e
sai-kurapati4 <saiganeshkurapati@gmail.com>
2026-06-28 11:49:48 +0530

    Spring Basic COnfiguration

---
+++ LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,12 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+	public void addBook() {
+		System.out.println("Book Service is working");
+		book.saveBook();}
+		    private BookRepository book;
+
+		    public void setBook(BookRepository book) {
+		        this.book = book;
+		    }
+	}
